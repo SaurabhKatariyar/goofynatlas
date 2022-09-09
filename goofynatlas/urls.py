effcaee994870481e6e15781a1c1446eb7f63962
Saurabh-Katariyar <saurabhkatariyar@gmail.com>
2022-09-09 22:44:45 +0530

    added login

--- goofynatlas/urls.py
+++ goofynatlas/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/pets', include('pets.urls')),
+    path('', include('rest_framework.urls')),
 ]
