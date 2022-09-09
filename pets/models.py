c1e832c7d5aeb488d4b2cd6ebf60e84cc7400ddd
Saurabh-Katariyar <saurabhkatariyar@gmail.com>
2022-09-09 21:02:02 +0530

    created pet register model

--- pets/models.py
+++ pets/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class PetRegister(models.Model):
+    pet_name = models.CharField(max_length=50)
+    pet_breed = models.CharField(max_length=50)
+    pet_owner_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    pet_image = models.ImageField()
+    pet_birth_date = models.DateField()
+    pet_register_date = models.DateTimeField(auto_now_add=True)
