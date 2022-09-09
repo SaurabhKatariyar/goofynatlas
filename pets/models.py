f75636548ae1d2d5cef4321a1d931a98770fc228
Saurabh-Katariyar <saurabhkatariyar@gmail.com>
2022-09-09 22:29:54 +0530

    corrected path.

--- pets/models.py
+++ pets/models.py
@@ -6,6 +6,6 @@ class PetRegister(models.Model):
     pet_type = models.CharField(max_length=50)
     pet_breed = models.CharField(max_length=50)
     pet_owner_name = models.ForeignKey(User, on_delete=models.CASCADE)
-    pet_image = models.ImageField(upload_to='/pets/images/')
+    pet_image = models.ImageField(upload_to='pets/images/')
     pet_birth_date = models.DateField()
     pet_register_date = models.DateTimeField(auto_now_add=True)
